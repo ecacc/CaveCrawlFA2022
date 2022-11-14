e468f841e4bb9301acf58c0cf19dd94f0d7c7f41
ecacc <emily.cacc@gmail.com>
2022-11-13 21:50:19 -0500

    Player Light + Basic Note
    The player emits light and it will slowly dim until the player gets a mushroom. There is a note that I was just trying out. There is paper that the player touches and then the note appears on the screen.

--- CaveCrawl/Assets/Scripts/ObjectCollect.cs
+++ CaveCrawl/Assets/Scripts/ObjectCollect.cs
@@ -9,12 +9,30 @@ public class ObjectCollect : MonoBehaviour
      public GameObject GC;
      public GameObject HS;
      public GameObject WB;
+     public GameObject light;
+     public static bool note = false;
+     public GameObject note1;
+     public GameObject inventory;
 
 
      void Start(){
+      note1.SetActive(false);
+      inventory.SetActive(true);
      }
 
-     void FixedUpdate(){
+     void Update(){
+      if(!note) {
+        light.GetComponent<UnityEngine.Rendering.Universal.Light2D>().intensity -= .0001f;
+      } else {
+        if(Input.GetKeyDown(KeyCode.E)) {
+          note = false;
+          note1.SetActive(false);
+          inventory.SetActive(true);
+        } else {
+          note1.SetActive(true);
+          inventory.SetActive(false);
+        }
+      }
      }
 
      void OnCollisionEnter2D(Collision2D other){
@@ -29,6 +47,10 @@ public class ObjectCollect : MonoBehaviour
             HS.SetActive(true);
           } else if (other.gameObject.tag == "WaterBucket"){
             WB.SetActive(true);
+          } else if (other.gameObject.tag == "mushroom") {
+            light.GetComponent<UnityEngine.Rendering.Universal.Light2D>().intensity = 1;
+          } else if (other.gameObject.tag == "note") {
+            note = true;
           }
      }
 }
