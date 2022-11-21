2b5b645ae371d5138c51e5c42638bbcb891d868f
ecacc <emily.cacc@gmail.com>
2022-11-21 13:03:09 -0500

    small edit to spider fight

--- CaveCrawl/Assets/Scripts/ArrowHit.cs
+++ CaveCrawl/Assets/Scripts/ArrowHit.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class ArrowHit : MonoBehaviour
 {
+    public int spiderHealth = 3;
+    public GameObject spiderHeart1;
+    public GameObject spiderHeart2;
+    public GameObject spiderHeart3;
+
+    void Start()
+    {
+        spiderHeart1.SetActive(true);
+        spiderHeart2.SetActive(true);
+        spiderHeart3.SetActive(true);
+    }
+
+    void Update()
+    {
+        if(spiderHealth == 2) {
+            spiderHeart3.SetActive(false);
+        } else if(spiderHealth == 1) {
+            spiderHeart2.SetActive(false);
+        } else if(spiderHealth == 0) {
+           spiderHeart1.SetActive(false); 
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D other){ 
         if(other.gameObject.tag == "arrow") {
             Destroy(other.gameObject);
+            spiderHealth -= 1;
             Bow.hit = true;
         }
     }
