dd937c9369a72e0cc2a7dfa2fe33186345a5ec29
samg-14 <sam.glaser@tufts.edu>
2022-11-27 13:19:16 -0700

    added grappling hook

--- CaveCrawl/Assets/Scripts/PlayerMove.cs
+++ CaveCrawl/Assets/Scripts/PlayerMove.cs
@@ -53,7 +53,7 @@ public class PlayerMove : MonoBehaviour {
             // NOTE: Switch player facing label
             FaceRight = !FaceRight;
 
-            // NOTE: Multiply player's x local scale by -1.
+        // NOTE: Multiply player's x local scale by -1.
             Vector3 theScale = transform.localScale;
             theScale.x *= -1;
             transform.localScale = theScale;
